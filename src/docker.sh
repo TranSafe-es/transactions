aaac567ef8e9e32d976d07157f3274f815dd56ad
Rafael Ferreira <mail@rafaelferreira.pt>
2016-11-06 20:42:29 +0000

    --restart always

--- src/docker.sh
+++ src/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker rm -f order-tracker
 docker build -t rfpt/order-tracker .
-docker run -d --name order-tracker -p 80:80 rfpt/order-tracker
+docker run -d --name order-tracker --restart always -p 80:80 rfpt/order-tracker
 
 # install:
 # apt-get -y install docker.io
