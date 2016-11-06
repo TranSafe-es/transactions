43de6ef9bc9692455f1ff1ec9025cff8b23d3026
Rafael Ferreira <mail@rafaelferreira.pt>
2016-11-06 14:59:09 +0000

    del vagrant add docker file

--- vagrant/server-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get install -y python-pip
-pip install -U pip setuptools
-cd /home/vagrant/order-tracker/src
-sudo pip install -r requirements.txt
-python manage.py migrate
-# python manage.py runserver 0.0.0.0:8000
-# acessivel em 192.168.33.11
