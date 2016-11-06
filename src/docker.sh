bb6a88a893b79f5fd9fed5f973bdaf542237910d
Rafael Ferreira <mail@rafaelferreira.pt>
2016-11-06 15:20:24 +0000

    docker run

---
+++ src/docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+docker rm -f order-tracker
+docker build -t rfpt/order-tracker .
+docker run -d --name order-tracker -p 80:80 rfpt/order-tracker
+
+# install:
+# apt-get -y install docker.io
+# ln -sf /usr/bin/docker.io /usr/local/bin/docker
+# sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+# update-rc.d docker.io defaults
